542187ee329bd2bfd0845ac515f28e0570cbfc41
egrzeszczak <ernest.grzeszczak@protonmail.com>
2023-09-18 20:20:57 +0200

    feat: detects IoCs, but extracts full URL with ips as IPv4 when ipv4 in url detected

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
+
+type IoC struct {
+	Value string
+	Type  string
+}
 
 // readIoCFromFile reads lines from a file and returns them as a slice.
 func readIoCFromFile(filePath string) ([]string, error) {
@@ -38,6 +44,62 @@ func readIoCFromFile(filePath string) ([]string, error) {
 	return lines, nil
 }
 
+// detectIoCTypes is a function for detecting IoC types and returns them as a slice of IoC structs.
+func detectIoCTypes(lines []string) []IoC {
+	var iocs []IoC
+
+	// Define regular expressions to match URLs, IPv4 addresses, domains, SHA1, SHA256, MD5, and email addresses.
+	urlPattern := `https?://[^\s]+`
+	urlRegex := regexp.MustCompile(urlPattern)
+	ipPattern := `\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`
+	ipRegex := regexp.MustCompile(ipPattern)
+	sha1Pattern := `[0-9a-fA-F]{40}`
+	sha1Regex := regexp.MustCompile(sha1Pattern)
+	sha256Pattern := `[0-9a-fA-F]{64}`
+	sha256Regex := regexp.MustCompile(sha256Pattern)
+	md5Pattern := `[0-9a-fA-F]{32}`
+	md5Regex := regexp.MustCompile(md5Pattern)
+	emailPattern := `[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}`
+	emailRegex := regexp.MustCompile(emailPattern)
+	domainPattern := `[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`
+	domainRegex := regexp.MustCompile(domainPattern)
+
+	// Loop through lines and check for IoCs.
+	for _, line := range lines {
+		if urlRegex.MatchString(line) {
+			// Detected URL
+			iocs = append(iocs, IoC{Value: line, Type: "URL"})
+		}
+	}
+
+	for _, line := range lines {
+		if ipRegex.MatchString(line) {
+			// Detected IPv4 Address
+			iocs = append(iocs, IoC{Value: line, Type: "IPv4"})
+		} else if sha1Regex.MatchString(line) {
+			// Detected SHA1 Hash
+			iocs = append(iocs, IoC{Value: line, Type: "SHA1"})
+		} else if sha256Regex.MatchString(line) {
+			// Detected SHA256 Hash
+			iocs = append(iocs, IoC{Value: line, Type: "SHA256"})
+		} else if md5Regex.MatchString(line) {
+			// Detected MD5 Hash
+			iocs = append(iocs, IoC{Value: line, Type: "MD5"})
+		} else if emailRegex.MatchString(line) {
+			// Detected Email Address
+			iocs = append(iocs, IoC{Value: line, Type: "Email"})
+		} else if domainRegex.MatchString(line) {
+			// Detected domain
+			iocs = append(iocs, IoC{Value: line, Type: "Domain"})
+		} else {
+			// It's an Unknown type
+			iocs = append(iocs, IoC{Value: line, Type: "Unknown"})
+		}
+	}
+
+	return iocs
+}
+
 func main() {
 	// Check if the correct number of command-line arguments is provided.
 	if len(os.Args) != 2 {
@@ -56,8 +118,16 @@ func main() {
 	}
 
 	// Print the lines or perform further processing as needed.
-	fmt.Println("Lines from the file:")
-	for _, line := range lines {
-		fmt.Println(line)
+	// fmt.Println("Lines from the file:")
+	// for _, line := range lines {
+	// 	fmt.Println(line)
+	// }
+
+	iocs := detectIoCTypes(lines)
+
+	for _, ioc := range iocs {
+		fmt.Printf("Type: %s, Value: %s\n", ioc.Type, ioc.Value)
 	}
+
+	// fmt.Printf("%v", detectIoCTypes(lines))
 }
