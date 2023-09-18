d52a02ecbe3bc2ff559d3c4e15fef0262e721d27
egrzeszczak <ernest.grzeszczak@protonmail.com>
2023-09-18 21:02:49 +0200

    feat: fixed some regexes and ifs, display only unique IoCs

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 type IoC struct {
 	Value string
 	Type  string
+	Raw   string
 }
 
 // readIoCFromFile reads lines from a file and returns them as a slice.
@@ -49,7 +50,7 @@ func detectIoCTypes(lines []string) []IoC {
 	var iocs []IoC
 
 	// Define regular expressions to match URLs, IPv4 addresses, domains, SHA1, SHA256, MD5, and email addresses.
-	urlPattern := `https?://[^\s]+`
+	urlPattern := `(https?://[^\s]+)`
 	urlRegex := regexp.MustCompile(urlPattern)
 	ipPattern := `\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}`
 	ipRegex := regexp.MustCompile(ipPattern)
@@ -61,39 +62,63 @@ func detectIoCTypes(lines []string) []IoC {
 	md5Regex := regexp.MustCompile(md5Pattern)
 	emailPattern := `[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}`
 	emailRegex := regexp.MustCompile(emailPattern)
-	domainPattern := `[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`
+	domainPattern := `[\w\.-]+\.[a-zA-Z]{2,}`
 	domainRegex := regexp.MustCompile(domainPattern)
+	emailDomainPattern := `@([\w\.-]+\.[a-zA-Z]{2,})`
+	emailDomainRegex := regexp.MustCompile(emailDomainPattern)
 
 	// Loop through lines and check for IoCs.
 	for _, line := range lines {
 		if urlRegex.MatchString(line) {
 			// Detected URL
-			iocs = append(iocs, IoC{Value: line, Type: "URL"})
-		}
-	}
+			urlMatches := urlRegex.FindStringSubmatch(line)
+			if len(urlMatches) > 1 {
+				url := urlMatches[1]
+				iocs = append(iocs, IoC{Value: url, Type: "URL", Raw: line})
+			}
 
-	for _, line := range lines {
-		if ipRegex.MatchString(line) {
+			// Extract IPv4 addresses from the URL
+			ipMatches := ipRegex.FindAllString(line, -1)
+			if len(ipMatches) > 0 {
+				for _, ip := range ipMatches {
+					iocs = append(iocs, IoC{Value: ip, Type: "IPv4", Raw: line})
+				}
+			} else {
+				// Extract Domains from the URL
+				domainMatch := domainRegex.FindAllString(line, -1)
+				for range domainMatch {
+					iocs = append(iocs, IoC{Value: domainMatch[0], Type: "Domain", Raw: line})
+				}
+			}
+
+		} else if ipRegex.MatchString(line) {
 			// Detected IPv4 Address
-			iocs = append(iocs, IoC{Value: line, Type: "IPv4"})
-		} else if sha1Regex.MatchString(line) {
-			// Detected SHA1 Hash
-			iocs = append(iocs, IoC{Value: line, Type: "SHA1"})
+			iocs = append(iocs, IoC{Value: line, Type: "IPv4", Raw: line})
 		} else if sha256Regex.MatchString(line) {
 			// Detected SHA256 Hash
-			iocs = append(iocs, IoC{Value: line, Type: "SHA256"})
+			iocs = append(iocs, IoC{Value: line, Type: "SHA256", Raw: line})
+		} else if sha1Regex.MatchString(line) {
+			// Detected SHA1 Hash
+			iocs = append(iocs, IoC{Value: line, Type: "SHA1", Raw: line})
 		} else if md5Regex.MatchString(line) {
 			// Detected MD5 Hash
-			iocs = append(iocs, IoC{Value: line, Type: "MD5"})
+			iocs = append(iocs, IoC{Value: line, Type: "MD5", Raw: line})
 		} else if emailRegex.MatchString(line) {
 			// Detected Email Address
-			iocs = append(iocs, IoC{Value: line, Type: "Email"})
+			iocs = append(iocs, IoC{Value: line, Type: "Email", Raw: line})
+
+			// Extract Domains from the URL
+			domainMatch := emailDomainRegex.FindStringSubmatch(line)
+			if len(domainMatch) > 1 {
+				iocs = append(iocs, IoC{Value: domainMatch[1], Type: "Domain", Raw: line})
+			}
+
 		} else if domainRegex.MatchString(line) {
 			// Detected domain
-			iocs = append(iocs, IoC{Value: line, Type: "Domain"})
+			iocs = append(iocs, IoC{Value: line, Type: "Domain", Raw: line})
 		} else {
 			// It's an Unknown type
-			iocs = append(iocs, IoC{Value: line, Type: "Unknown"})
+			iocs = append(iocs, IoC{Value: line, Type: "Unknown", Raw: line})
 		}
 	}
 
@@ -117,17 +142,22 @@ func main() {
 		return
 	}
 
-	// Print the lines or perform further processing as needed.
-	// fmt.Println("Lines from the file:")
-	// for _, line := range lines {
-	// 	fmt.Println(line)
-	// }
+	// Create a map to store unique IoC pairs.
+	uniqueIoCs := make(map[string]IoC)
 
+	// Detect IoC types and add them to the map.
 	iocs := detectIoCTypes(lines)
-
 	for _, ioc := range iocs {
-		fmt.Printf("Type: %s, Value: %s\n", ioc.Type, ioc.Value)
+		// Generate a unique key by combining Type and Value.
+		key := fmt.Sprintf("%s-%s", ioc.Type, ioc.Value)
+		// Check if the IoC is not already in the map before adding it.
+		if _, exists := uniqueIoCs[key]; !exists {
+			uniqueIoCs[key] = ioc
+		}
 	}
 
-	// fmt.Printf("%v", detectIoCTypes(lines))
+	// Print the unique IoC pairs.
+	for _, ioc := range uniqueIoCs {
+		fmt.Printf("(%s)   \t %s\n", ioc.Type, ioc.Value)
+	}
 }
